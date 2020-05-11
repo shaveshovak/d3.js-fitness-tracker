88f6b09ba0b3961d629a6232abc3bb216032aca2
Khatuna <9b9de9fa>
2020-05-11 23:42:33 +0400

    add styles

---
+++ index.js
@@ -0,0 +1,21 @@
+// DOM elements
+const btns = document.querySelectorAll('button');
+const form = document.querySelector('form');
+const formAct = document.querySelector('form span');
+const input = document.querySelector('input');
+const error = document.querySelector('.error');
+
+var activity = 'cycling';
+
+btns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        // get activity
+        activity = e.target.dataset.activity;
+
+        // remove and add active class
+        btns.forEach(btn => btn.classList.remove('active'));
+        e.target.classList.add('active');
+
+        
+    })
+});
